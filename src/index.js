80a0930a818e8236cfd086851570cf3b5195e9b4
fortune <fortune>
2024-06-15 17:48:02 +0800

    Navigator框架搭建中

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { StyleSheet } from "react-native";
+
+export default class index extends Component{
+    render(){
+        return (
+            <View>
+                
+            </View>
+        )
+    }
+}
+
+const styles =StyleSheet.create({})
---
+++ src/routes/HomeStack.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import { StyleSheet, View } from "react-native";
+import { createStackNavigator} from'@react-navigation/stack'
+
+const Stack = createStackNavigator()
+
+export default class HomeStack extends Component {
+    render() {
+        return (
+           <View>
+            
+           </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({})
