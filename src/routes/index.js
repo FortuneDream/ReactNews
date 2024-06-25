cbea7bb50d82fbdd658686674571fc084e2ae10e
fortune <fortune>
2024-06-25 09:16:23 +0800

    编译运行通过

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet,Text } from "react-native";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { Ionicons } from 'react-native-vector-icons/Ionicons'
 import HomeScreen from './HomeStack'
@@ -12,23 +12,26 @@ const Tab = createBottomTabNavigator()
 export default class index extends Component {
     render() {
         return (
-            
+
             <Tab.Navigator
                 screenOptions={({ route }) => ({
                     tabBarIcon: ({ focused, color, size }) => {
                         let iconName
                         if (route.name === 'Home') {
                             iconName = focused ? 'add-circle' : 'add-circle-outline'
-                        } else if (route.name = 'News') {
-                            iconName = focused ? 'person' : 'person-outline'
-                        }
-                        return <Ionicons name={iconName} size={size} color={color} />
+                        } else if (route.name === 'News') {
+                            iconName = focused ? 'settings' : 'settings-outline'
+                        } else if (route.name === 'User'){
+                            iconName = focused ? 'person' : "person-outline"
+                        } 
+                        // <Ionicons name={iconName} size={size} color={color} /> 无法使用，原因未知
+                        return <Text name={iconName} />
                     }
                 })}
-                >
-                    <Tab.Screen name="Home" component={HomeScreen} />
-                    <Tab.Screen name="News" component={NewsScreen} />
-                    <Tab.Screen name="User" component={UserScreen} />
+            >
+                <Tab.Screen name="Home" component={HomeScreen} />
+                <Tab.Screen name="News" component={NewsScreen} />
+                <Tab.Screen name="User" component={UserScreen} />
             </Tab.Navigator>
         )
     }
