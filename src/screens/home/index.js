d51ade88bda28819142f89821e37a74a0101d22b
fortune <fortune>
2024-09-22 09:57:32 +0800

    首页中，完成

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,7 +1,54 @@
 import React, { Component } from 'react'
 import Swiper from 'react-native-swiper'
-import { Text, View, Image, ScrollView, StyleSheet, Touchable, TouchableOpacity, Dimensions } from 'react-native'
+import { Text, View, Image, ScrollView, StyleSheet, Touchable, TouchableOpacity, Dimensions, Alert, FlatList } from 'react-native'
+import Geolocation from '@react-native-community/geolocation'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 export class index extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            city: "北京",
+            indeces: [
+                {
+                    type: '1',
+                    name: "name1"
+                }, {
+                    type: '2',
+                    name: "name2"
+                }, {
+                    type: '3',
+                    name: "name3"
+                }, {
+                    type: '4',
+                    name: "name3"
+                }]
+        }
+    }
+
+    componentDidMount() {
+        this.setState({
+            city: "上海"
+        })
+    }
+
+    indecesItem = ({ index, item }) => {
+        return (
+            <TouchableOpacity
+                key={'index' + item.type}
+                onPress={() => {
+                    alert(item.type)
+                }}>
+                <View style={[styles.indexItem]}>
+                    <Text style={[styles.indexName]}>
+                        {item.name}
+                    </Text>
+                </View>
+
+            </TouchableOpacity>
+        )
+    }
+
     render() {
         return (
             <View>
@@ -44,6 +91,19 @@ export class index extends Component {
                         <Image style={[styles.slideImage]}
                             source={require('../../images/4.jpeg')} />
                     </Swiper>
+                    <View style={[styles.city]}>
+                        <Text style={[styles.cityText]}>
+                            {this.state.city}
+                        </Text>
+                    </View>
+                    <View style={[styles.indexContainer]}>
+                        <FlatList
+                            data={this.state.indeces}
+                            renderItem={this.indecesItem}
+                            keyExtractor={item => item.type}
+                            horizontal={true}
+                        />
+                    </View>
                 </ScrollView>
             </View>
         )
@@ -72,7 +132,29 @@ const styles = StyleSheet.create({
     slideImage: {
         height: 200,
         width: Dimensions.get('window').width,
-        resizeMode :'center'
+        resizeMode: 'center'
+    },
+    city: {
+        flex: 1,
+        justifyContent: 'center'
+    },
+    cityText: {
+        fontSize: 24,
+        marginHorizontal: 10
+    },
+    indexItem:{
+        alignItems:'center',
+        justifyContent:'center',
+        borderRadius:20,
+        backgroundColor:'#deb',
+        width:Dimensions.get('window').width/3 -10,
+        height:80,
+        marginTop:10,
+        marginRight:10
+    },
+    indexName:{
+        color:'#222',
+        fontSize:14
     }
 })
 
