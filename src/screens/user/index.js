b34c95e28fd91c2cce3f60b3a0b9745c49e9aa71
fortune <fortune>
2024-09-28 10:11:10 +0800

    Redux部分没学了，视频课程比较拉

--- src/screens/user/index.js
+++ src/screens/user/index.js
@@ -1,14 +1,19 @@
-import React, {Component} from 'react'
-import {Text,View} from 'react-native'
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+import { TouchableOpacity } from 'react-native-gesture-handler'
 
-export class index extends Component{
-render(){
-    return (
-        <View>
-            <Text>User</Text>
-        </View>
-    )
-}
+export class index extends Component {
+    render() {
+        return (
+            <View>
+                <TouchableOpacity
+                    onPress={() => this.props.navigation.navigate('Abount')}>
+                    <Text>User</Text>
+                </TouchableOpacity>
+
+            </View>
+        )
+    }
 }
 
 export default index
